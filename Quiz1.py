144a05a55fb3f99bcbbf3ed16f3dabb6feb23bef
Jackie <JGtheophila19@gmail.com>
2018-10-13 19:30:20 -0400

    practice 1

---
+++ Quiz1.py
@@ -0,0 +1,32 @@
+# This is a Python comment. Lines that begin with a '#' are ignored by the
+# Python interpreter. Comments are useful for documenting code or explaining
+# quiz questions!
+# 
+# Write a Python program that prints out the number of minutes in seven weeks.
+# Remember: 7 weeks 7 days in a week, 24 hours in a day, and 60 mins in an hour.
+# Multiplying these numbers together will give you the result
+#
+# Click the "Test Run" button below to try running your code and see the output,
+# and click "Submit" to submit your answer.
+
+# WEEKS: 1 2 3 4 5 6 7 
+# ---------------------
+#        1  24hr. 1hr = 60min
+# D      2  |
+# A      3  |
+# Y      4  |
+# S      5  |
+#        6  |
+#        7  24hrs.
+
+
+# 24 hours x 7 days
+
+print 24 * 7
+# = 168 hours in 7 days
+
+# 168 hrs * 60 min = 10,080
+# 10080 * 7 days
+
+print 168 * 60 * 7
+# 70560 min in 7 days
