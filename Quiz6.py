a7806ffef160f5d951225d555318c6b5abe53328
Jackie <JGtheophila19@gmail.com>
2018-10-20 11:25:05 -0400

    Selecting Sub Sequences

---
+++ Quiz6.py
@@ -0,0 +1,7 @@
+# Write Python code that prints out Udacity (with a capital U), given the definition of s below.
+
+s = 'audacity'
+
+print "U" + s[2] + s[3] + s[4] + s[5] + s[6] + s[7]
+
+# Udacity
