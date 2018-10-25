bc54a985b169d15bc4cf2c4d4b531e6659c06f3d
Jackie <JGtheophila19@gmail.com>
2018-10-24 23:01:37 -0400

    Multiplication in python

--- Quiz1.py
+++ Quiz1.py
@@ -1,23 +1,22 @@
-# This is a Python comment. Lines that begin with a '#' are ignored by the
-# Python interpreter. Comments are useful for documenting code or explaining
-# quiz questions!
-# 
+# This is a Python comment. Lines that begin with a '#' are ignored by the Python interpreter. 
+# Comments are useful for documenting code or explaining quiz questions!
+
 # Write a Python program that prints out the number of minutes in seven weeks.
 # Remember: 7 weeks 7 days in a week, 24 hours in a day, and 60 mins in an hour.
 # Multiplying these numbers together will give you the result
-#
+
 # Click the "Test Run" button below to try running your code and see the output,
 # and click "Submit" to submit your answer.
 
-# WEEKS: 1 2 3 4 5 6 7 
-# ---------------------
-#        1  24hr. 1hr = 60min
-# D      2  |
-# A      3  |
-# Y      4  |
-# S      5  |
-#        6  |
-#        7  24hrs.
+#   WEEKS: 1 2 3 4 5 6 7 
+#   ---------------------
+#          1  24hr. 1hr = 60min
+#   D      2  |
+#   A      3  |
+#   Y      4  |
+#   S      5  |
+#          6  |
+#          7  24hrs.
 
 
 # 24 hours x 7 days
