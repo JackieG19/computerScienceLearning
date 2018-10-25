8e782158dc941d807e37fca9a257f177b5b3be3f
Jackie <JGtheophila19@gmail.com>
2018-10-24 23:06:21 -0400

    Using Strings

--- Quiz5.py
+++ Quiz5.py
@@ -1,12 +1,12 @@
-# Write python code that defines the variable 
-# age to be your age in years, and then prints 
-# out the number of days you have been alive.
+# Write python code that defines the variable age to be your age in years, 
+# and then prints out the number of days you have been alive.
 
 age = 28
 days = 365
-print age * days
 
---- Quiz 5
+print age * days
+# 28 * 365 = 10,220
+
 # Define a variable, name, and assign to it a string that is your name.
 
 name = "Jackie"
